8ac754e478ecb72c72695848bb7908923cb34369
Nazilafitria <112499349+Nazilafitria@users.noreply.github.com>
2025-01-12 15:07:18 +0700

    main.dart

--- main.dart
+++ main.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
-import 'splash.dart';
+import 'splash.dart'; // Halaman Splash
+import 'halamanlogin.dart'; // Halaman Login
+import 'signup.dart'; // Halaman Sign Up
+import 'home.dart'; // Halaman Home setelah login
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      title: 'Flutter App', // Judul aplikasi
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // Warna utama aplikasi
+      ),
+      initialRoute: '/', // Rute awal, menampilkan halaman SplashPage
+      routes: {
+        '/': (context) => const SplashPage(), // Halaman Splash sebagai default
+        '/login': (context) => const LoginPage(), // Halaman Login
+        '/signup': (context) => const SignUpPage(), // Halaman Sign Up
+        '/home': (context) => const HomePage(), // Halaman utama setelah login
+      },
     );
   }
 }
